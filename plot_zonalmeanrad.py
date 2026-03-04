1d92e37ff7483a86dff0ed3e751a35a8c041c237
luciadesanmed <luciadesanmed@gmail.com>
2026-03-04 22:18:54 +0100

    plot_zonalmeanrad.py

--- plot_zonalmeanrad.py
+++ plot_zonalmeanrad.py
@@ -6,53 +6,56 @@ from pathlib import Path
 
 # Data path
 era_path = Path("/home/desan/ESM-data/")
-top_sw_down = era_path / 'zonal_avg_top_sw_down.nc'
+top_net_sw = era_path / 'net-short-annual-Zonmean.nc'
+top_net_lw = era_path / 'net-long-annual-Zonmean.nc'
 
-data = xr.open_dataset(top_sw_down)
+cases = (top_net_sw, top_net_lw)
+varc = ('avg_tnswrf', 'avg_tnlwrf')
 
-var = data['avg_tdswrf']
-var = var.squeeze() #12x721
-var = var.transpose()
-var = np.flipud(var)
-var = np.ma.masked_where(var <= 0, var)
-levels = np.arange(0, 650, 50)
-
-cmap_base = plt.cm.jet
-colors = cmap_base(np.linspace(0, 1, len(levels)-1))
-colors[0] = [1, 1, 1, 1]
-cmap = mcolors.ListedColormap(colors)
-#cmap.set_under('white')
-#cmap.set_bad('white')
+n=0
+var = [  ]
+for i in cases:
+    data = xr.open_dataset(i)
+    dat = data[varc[n]].squeeze()
+    var.append(dat)
+    n = n+1
 
 lat = np.arange(-90.0, 90.25, 0.25) #721
-date = np.arange(0, 12, 1) #12
 
-plt.figure(figsize=(10, 10))
-ax = plt.axes()
-ax.set_facecolor('white')
-ax.set_box_aspect(1)
+fz = 12
 
-cs = plt.contour(date, lat, var, 10,
-    colors='k',
-    linewidths=0.5,
-    add_colorbar = False)
+fig, ax = plt.subplots(2)
+#fig.tight_layout()
+ax[0].plot(lat, var[0],
+           color='b',
+           linestyle='-')
+ax[0].plot(lat, -1.0*var[1],
+           color='r',
+           linestyle='--')
+ax[0].set_xlabel("Latitude", fontsize=fz)
+ax[0].set_ylabel("Radiation" +  r" (W m$^{-2}$)", fontsize=fz)
+ax[0].set_yticks(np.arange(30,330.1,30))
+ax[0].set_xticks(np.arange(-90, 90.1, 30))
+ax[0].set_xticklabels(['90S','60S','30S','EQ','30N','60N','90N'], fontsize=fz)
+ax[0].set_xlim(-90, 90)
+ax[0].set_ylim(30,330)
+ax[0].set_xlabel('Latitude', fontsize=fz)
+ax[0].legend(['Absorbed solar radiation','Outgoing longwave radiation'],
+             edgecolor='white')
+ax[0].set_title('Radiation zonal mean at the TOA in annual mean',
+                fontweight='bold')
 
-cf = plt.contourf(date, lat, var,
-             levels = levels,
-             cmap=cmap,
-             extend='neither')
-
-fz=16
-plt.xlabel("Month", fontsize=fz)
-plt.ylabel("Latitude", fontsize=fz)
-plt.title("Zonal mean based on daily insolation", fontsize=fz, fontweight='bold')
-cbar = plt.colorbar(cf, ticks=[0] + list(levels))
-cbar.set_label(r"W m$^{-2}$", fontsize=fz)
-cbar.ax.tick_params(labelsize=fz)
-ax.set_xticks(np.arange(0,12,1))
-ax.set_xticklabels(['J','F','M','A','M','J','J','A','S','O','N','D'], fontsize=fz)
-ax.set_yticks(np.arange(-90,90.25,30))
-ax.set_yticklabels(['90S','60S','30S','0','30N','60N','90N'], fontsize=fz)
-
-plt.savefig('zonalmean.png')
-
+ax[1].plot(lat, var[0]- (-1.0*var[1]),
+           color='g',
+           linestyle='-')
+ax[1].set_yticks(np.arange(-140, 80, 20))
+ax[1].set_xticks(np.arange(-90, 90.1, 30))
+ax[1].set_xticklabels(['90S','60S','30S','EQ','30N','60N','90N'], fontsize=fz)
+ax[1].set_xlim(-90, 90)
+ax[1].set_ylabel("Radiation" +  r" (W m$^{-2}$)", fontsize=fz)
+ax[1].set_xlabel('Latitude', fontsize=fz)
+ax[1].set_title('Absorbed solar radiation - Outgoing longwave radiation', 
+                fontweight='bold')
+plt.subplots_adjust(hspace=0.5)
+plt.savefig('rad-zonalmean.png')
+#plt.show()
