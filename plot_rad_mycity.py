220280fc2741f6aa95e3ada0b23b7bfc8ee1d5af
luciadesanmed <luciadesanmed@gmail.com>
2026-03-09 20:18:25 +0100

    plot_rad_mycity.py

--- plot_rad_mycity.py
+++ plot_rad_mycity.py
@@ -6,55 +6,53 @@ from pathlib import Path
 
 # Data path
 era_path = Path("/home/desan/ESM-data/")
-net_lw = era_path / 'surface-net-lw.nc'
-net_sw = era_path / 'surface-net-sw.nc'
-net_sh = era_path / 'surface-sensible-heat.nc'
-net_lh = era_path / 'surface-latent-heat.nc'
+net_rad = era_path / 'ags-avg-net-surface-rad.nc'
+net_sh = era_path / 'ags-avg-sensible-heat.nc'
+net_lh = era_path / 'ags-avg-surface-latent-heat.nc'
+ht_str = era_path / 'ags-avg-heat-storage.nc'
 
-cases = (net_lw, net_sw, net_sh, net_lh)
-varc = ('avg_snlwrf', 'avg_snswrf', 'avg_ishf', 'avg_slhtf')
+cases = (net_rad, net_lh, net_sh, ht_str)
+varc = ('ssr', 'avg_slhtf','avg_ishf', 'ssr')
 
-latmin =  21.5139417 #21°30'50.19"N
-latmax =  22.4147556 #22°24'53.12"N
-lonmin = 257.052264 #102°56'51.85"O
-lonmax = 258.204600 #101°47'43.44"O
+#latmin =  21.5139417 #21°30'50.19"N
+#latmax =  22.4147556 #22°24'53.12"N
+#lonmin = 257.052264 #102°56'51.85"O -102.947736
+#lonmax = 258.204600 #101°47'43.44"O -101.7953997
 
 n=0
 var = [  ]
-#for i in cases:
-data = xr.open_dataset(cases[0])
-dat = data[varc[0]].squeeze()
-#Chop for my area:
-dat_sub = dat.sel(latitude=slice(latmax, latmin), longitude=slice(lonmin, lonmax))
+for i in cases:
+    data = xr.open_dataset(i)
+    dat = data[varc[n]].squeeze()
+    var.append(dat)
+    n = n+1
 
-    #var.append(dat)
-    #n = n+1
+mth = np.arange(1, 12.1, 1) #12 months
 
-#lat = np.arange(-90.0, 90.25, 0.25) #721
+fz = 12
 
-#fz = 12
-
-#fig, ax = plt.subplots(2)
-#fig.tight_layout()
-#ax[0].plot(lat, var[0],
-#           color='b',
-#           linestyle='-')
-#ax[0].plot(lat, -1.0*var[1],
-#           color='r',
-#           linestyle='--')
-#ax[0].set_xlabel("Latitude", fontsize=fz)
-#ax[0].set_ylabel("Radiation" +  r" (W m$^{-2}$)", fontsize=fz)
-#ax[0].set_yticks(np.arange(30,330.1,30))
-#ax[0].set_xticks(np.arange(-90, 90.1, 30))
-#ax[0].set_xticklabels(['90S','60S','30S','EQ','30N','60N','90N'], fontsize=fz)
-#ax[0].set_xlim(-90, 90)
-#ax[0].set_ylim(30,330)
-#ax[0].set_xlabel('Latitude', fontsize=fz)
-#ax[0].legend(['Absorbed solar radiation','Outgoing longwave radiation'],
-#             edgecolor='white')
-#ax[0].set_title('Radiation zonal mean at the TOA in annual mean',
-#                fontweight='bold')
+plt.plot(mth, var[0],
+          color='k',
+           linestyle='-')
+plt.plot(mth, -1*var[1],
+           color='b',
+           linestyle='--')
+plt.plot(mth, -1*var[2],
+         color='r',
+         linestyle='--')
+plt.plot(mth, var[3],
+         color='y',
+         linestyle='--')
+plt.xlabel("Month", fontsize=fz, fontweight='bold')
+plt.ylabel(r" W m$^{-2}$", fontsize=fz, fontweight='bold')
+plt.yticks(np.arange(-50,300.1,50))
+plt.xticks(np.arange(1, 12.1, 1), labels=['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'], fontsize=fz)
+plt.ylim(-49, 300)
+plt.legend(['Rn','LE','SH','G'],
+             edgecolor='white')
+plt.title('Radiation fluxes (ERA5 1991-2020) - Aguascalientes, Mexico',
+                fontweight='bold')
 
 
-#plt.savefig('rad-zonalmean.png')
+plt.savefig('ags-radiation.png')
 #plt.show()
