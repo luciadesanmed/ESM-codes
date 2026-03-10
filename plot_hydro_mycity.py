b8222187acb4a28b0c5446ee2b07e5356640b3d2
luciadesanmed <luciadesanmed@gmail.com>
2026-03-10 11:21:02 +0100

    plot_hydro_mycity.py

--- plot_hydro_mycity.py
+++ plot_hydro_mycity.py
@@ -9,9 +9,10 @@ era_path = Path("/home/desan/ESM-data/")
 pet = era_path / 'ags-avg-pot-evap.nc'
 p = era_path / 'ags-avg-prec.nc'
 e = era_path / 'ags-avg-evap.nc'
+temp = era_path / 'ags-avg-temp.nc'
 
-cases = (pet, p, e)
-varc = ('pev', 'tp','e')
+cases = (pet, p, e, temp)
+varc = ('pev', 'tp','e', 't2m')
 
 #latmin =  21.5139417 #21°30'50.19"N
 #latmax =  22.4147556 #22°24'53.12"N
@@ -29,23 +30,30 @@ for i in cases:
 mth = np.arange(1, 12.1, 1) #12 months
 
 fz = 12
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
 
-plt.plot(mth, -1000*var[0],
+ax1.plot(mth, -1000*var[0],
           color='k',
            linestyle='-')
-plt.plot(mth, 1000*var[1],
+ax1.plot(mth, 1000*var[1],
            color='b',
            linestyle='--')
-plt.plot(mth, -1000*var[2],
+ax1.plot(mth, -1000*var[2],
          color='r',
          linestyle='--')
-plt.xlabel("Month", fontsize=fz, fontweight='bold')
-plt.ylabel(r" mm day$^{-1}$", fontsize=fz, fontweight='bold')
+ax1.set_xlabel("Month", fontsize=fz, fontweight='bold')
+ax1.set_ylabel(r" mm day$^{-1}$", fontsize=fz, fontweight='bold')
 #plt.yticks(np.arange(-50,300.1,50))
-plt.xticks(np.arange(1, 12.1, 1), labels=['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'], fontsize=fz)
+ax1.set_xticks(np.arange(1, 12.1, 1), labels=['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'], fontsize=fz)
 #plt.ylim(-49, 300)
-plt.legend(['PET','P','E'],
+ax2.plot(mth, var[3]-273.15,
+         color='y',
+         linestyle='--')
+ax1.legend(['PET','P','E'],
              edgecolor='white')
+ax2.set_ylabel('Temperature (°C)', fontsize=fz, fontweight='bold', color='y')
+#ax1.legend('T', edgecolor='white')
 plt.title('Terrestrial water balance (ERA5 1991-2020) \nAguascalientes, Mexico',
                 fontweight='bold')
 
